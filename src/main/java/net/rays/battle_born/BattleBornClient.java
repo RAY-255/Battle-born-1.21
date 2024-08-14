9f2ffe6d2a84812c41a38482e3e15d882a52aef1
Ray <pacmanguris@gmail.com>
2024-08-14 13:11:38 +0200

    1

---
+++ src/main/java/net/rays/battle_born/BattleBornClient.java
@@ -0,0 +1,10 @@
+package net.rays.battle_born;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BattleBornClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/rays/battle_born/BattleBornDataGenerator.java
@@ -0,0 +1,11 @@
+package net.rays.battle_born;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BattleBornDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
